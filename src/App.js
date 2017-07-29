666523a66a8e2dbef561e5102927c11aa61d7a00
Gilberto Villa <ingilniero@gmail.com>
2017-07-29 18:10:06 -0500

    Add routing
    - Add react-router-dom package
    - Update App to handle the index and search routes
    - Update BooksList component with a Link to the search page
    - Update Search component with a Link to the home page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,12 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
+import { Route } from 'react-router-dom'
 import groupBy from 'group-by'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -31,11 +25,13 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks books={books}/>
-        ) : (
+        <Route exact path='/' render={() => (
           <ListBooks books={groupedBooks}/>
-        )}
+        )} />
+
+        <Route path='/search' render={() => (
+          <SearchBooks books={books}/>
+        )} />
       </div>
     )
   }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import bookshelves from './bookshelves'
 import Bookshelf from './Bookshelf'
+import { Link } from 'react-router-dom'
 
 class ListBooks extends Component {
   static propTypes = {
@@ -33,7 +34,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
